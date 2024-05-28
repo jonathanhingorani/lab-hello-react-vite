3209808b7a4e0d25b9dd8b445170015dadce20ac
Jonathan Hingorani <jonathanhingorani@gmail.com>
2024-05-28 02:09:16 +0200

    attempted

---
+++ hello-project/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { Info } from './components/Info';
+import { Navbar } from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <div className="Navbar"></div>
+      <Info/>
+      <div className="Info"></div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ hello-project/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from '../assets/ironhack-logo-xs.png'
+import menu from '../assets/menu-top-xs.png'
+
+
+export const Navbar = () => {
+  return ( 
+    <div className='navbar'>
+        <img src={logo} alt='ironhack logo' style={{}}/>
+        <img src={menu} alt='menu' style={{}}/>
+    </div> 
+  )
+
+    
+  
+}
